58bea44d4348e7d054f679c77f216f3052021d0c
noobad_lunatic <mhtbelokars@gmail.com>
2019-03-24 00:13:50 +0530

    First Project

---
+++ src/eventmanagementsystem/EventManagementSystem.java
@@ -0,0 +1,30 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventmanagementsystem;
+import java.sql.*;
+import java.*;
+
+/**
+ *
+ * @author Mohit
+ */
+public class EventManagementSystem {
+    public static LogIn b= new LogIn();
+    
+    
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        b.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
